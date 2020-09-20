75ac777c1c53ae57a62a348f2d0e330d744df828
syree <harshchaudharyp1cleaner@gmail.com>
2020-09-20 16:40:12 +0530

    first commit

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,113 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  final Function toggleView;
+  SignIn({this.toggleView});
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  // text field state
+  bool loading = false;
+  String email = "";
+  String password = "";
+  final _formKey = GlobalKey<FormState>();
+  String error = "";
+  // instance of authService class
+  final AuthService _auth = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return loading == true
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign In to Brew Crew'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text('Register')),
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val.isEmpty ? "Enter an email" : null,
+                        onChanged: (val) {
+                          setState(() {
+                            return email = val;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        validator: (val) => val.length < 6
+                            ? "Enter a password 6+ chars long"
+                            : null,
+                        obscureText: true,
+                        onChanged: (val) => password = val,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      RaisedButton(
+                        color: Colors.pink[400],
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() {
+                              loading = true;
+                            });
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error =
+                                    'Could not sign in with those credentials';
+                                loading = false;
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      ),
+                    ],
+                  ),
+                )),
+          );
+  }
+}
